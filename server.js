9b628b2b62c050c1753485e99d24b0763e2905f7
Manuel Stofer <manuelstofer@gmail.com>
2013-04-01 23:55:49 +0200

    Export client for node module too

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 module.exports = require('./src/storage');
+module.exports.client = require('./src/client/client');
 module.exports.backends = {
     memory: require('./src/backends/memory'),
     mongo: require('./src/backends/mongo')
